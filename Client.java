40ec8c2d5902c7795162ae7e2c6251d9e948594c
Varun Gupta <varun@Varuns-MacBook-Pro.local>
2019-09-05 14:57:26 +0530

    BT

--- Client.java
+++ Client.java
@@ -28,21 +28,20 @@ class Client {
     private void sendMessage(String to , String message) throws Exception
     {
         DataOutputStream outToServer = new DataOutputStream(clientSocketSen.getOutputStream());
-        System.out.println(clientSocketSen);
         outToServer.writeBytes("SEND " + to + "\nContent-length: " + message.length() + "\n\n" + message);
-        // outToServer.writeBytes("SEND " + to);
-        // BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocketSen.getInputStream()));
-        // String response = inFromServer.readLine();
-        // String[] splitRes = response.split(" ");
-        // if(!splitRes[0].equals("SENT")){
-        //     if(splitRes[0].equals("ERROR") && splitRes[1] == "102"){
-        //         System.out.println("Unable To Send ....");
-        //     }else{
-        //         System.out.println("Header Incomplete ....");
-        //     }
-        // }else{
-        //     System.out.println("Message Sent Successfully ....!!");
-        // }
+        outToServer.writeBytes("SEND " + to);
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocketSen.getInputStream()));
+        String response = inFromServer.readLine();
+        String[] splitRes = response.split(" ");
+        if(!splitRes[0].equals("SENT")){
+            if(splitRes[0].equals("ERROR") && splitRes[1] == "102"){
+                System.out.println("Unable To Send ....");
+            }else{
+                System.out.println("Header Incomplete ....");
+            }
+        }else{
+            System.out.println("Message Sent Successfully ....!!");
+        }
     }
 
     private void registerToSend() throws Exception
@@ -51,13 +50,12 @@ class Client {
         System.out.println("User Name:");
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         clientSocketSen = new Socket(hostIP, port);
-        System.out.println(clientSocketSen);
+        
         DataOutputStream outToServer = new DataOutputStream(clientSocketSen.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocketSen.getInputStream()));
         userName = inFromUser.readLine();
         outToServer.writeBytes("REGISTER TOSEND " + userName + "\n\n");
         String response = inFromServer.readLine();
-        System.out.println(response);
         String[] splitRes = response.split(" ");
         if(!(splitRes[0].equals("REGISTERED") && splitRes[1].equals("TOSEND") && splitRes[2].equals(userName))){
             System.out.println("NOT A VALID USER NAME OR USERNAME ALREADY REGISTERED!!!");
@@ -72,9 +70,8 @@ class Client {
         clientSocketRec = new Socket(hostIP, port);
         DataOutputStream outToServer = new DataOutputStream(clientSocketRec.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocketRec.getInputStream()));
-        outToServer.writeBytes("REGISTER TORECV " + userName + "\n");
+        outToServer.writeBytes("REGISTER TORECV " + userName + "\n\n");
         String response = inFromServer.readLine();
-        System.out.println(response);
         String[] splitRes = response.split(" ");
         if(!(splitRes[0].equals("REGISTERED") && splitRes[1].equals("TORECV") && splitRes[2].equals(userName))){
             clientSocketRec.close();
@@ -86,29 +83,37 @@ class Client {
 class Receiver extends Thread{
     public Socket socketRec;
     public BufferedReader inFromServer;
+    DataOutputStream outToServer;
     public Receiver(Socket socket) throws Exception{
         socketRec = socket;
         inFromServer = new BufferedReader(new InputStreamReader(socketRec.getInputStream()));
-
+        outToServer = new DataOutputStream(socketRec.getOutputStream());
     }
     @Override
     public void run(){
         try{
             while(true){
-                
-                String finalMsg = "";
-                String response = inFromServer.readLine();
-                String[] splitRes = response.split(" ");
-                finalMsg += splitRes[1];
-                response = inFromServer.readLine();
-                splitRes = response.split(": ");
-                int contentLength = Integer.parseInt(splitRes[1]);
-                response = inFromServer.readLine();
-                char[] message = new char[contentLength];;
-                inFromServer.read(message , 0 , contentLength);
-                response = String.valueOf(message);
-                finalMsg += ": " + response;
-                System.out.println(finalMsg);
+                try{
+                    String finalMsg = "";
+                    String sender = "";
+                    String response = inFromServer.readLine();
+                    String[] splitRes = response.split(" ");
+                    finalMsg += splitRes[1];
+                    sender = splitRes[1];
+                    response = inFromServer.readLine();
+                    splitRes = response.split(": ");
+                    int contentLength = Integer.parseInt(splitRes[1]);
+                    response = inFromServer.readLine();
+                    char[] message = new char[contentLength];;
+                    inFromServer.read(message , 0 , contentLength);
+                    response = String.valueOf(message);
+                    finalMsg += ": " + response;
+                    
+                    outToServer.writeBytes("RECEIVED hi\n\n");
+                    System.out.println(finalMsg);
+                }catch(Exception e){
+                    outToServer.writeBytes("ERROR 103 Header incomplete");
+                }   
             }
         }
         catch(Exception e){
