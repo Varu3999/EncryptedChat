fe42aa8559ef469e537e05f2f79fc25cb48e3a81
Varun Gupta <varun@Varuns-MacBook-Pro.local>
2019-09-07 02:30:58 +0530

    changes

--- Encrypted version/Client.java
+++ Encrypted version/Client.java
@@ -78,7 +78,7 @@ class Client {
         inFromServer.read(messagec , 0 , keyLen);
         response = String.valueOf(messagec);
         byte[] encrytpMsg = encrypt(response.getBytes(), message.getBytes());
-        message = Base64.getEncoder().encodeToString(encrytpMsg);
+        // message = Base64.getEncoder().encodeToString(encrytpMsg);
         outToServer.writeBytes("SEND " + to + "\nContent-length: " + message.length() + "\n\n" + message);
         // outToServer.writeBytes("SEND " + to);
         
